27acc5ba1488e7b8735a93c6927061074c385da7
londony11 <londony11@gmail.com>
2023-10-24 06:47:27 +0200

    start add logo funcitonality

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class App:
         Button(text="Add Text", command=self.add_text).grid(column=1, row=3, sticky="nesw")
 
         # Add Logo button
-        Button(text="Add Logo").grid(column=2, row=3, sticky="nesw")
+        Button(text="Add Logo", command=self.add_logo).grid(column=2, row=3, sticky="nesw")
 
 
     def open_photo(self):
@@ -62,23 +62,53 @@ class App:
 
         text_menu = Toplevel(window)
         text_menu.title("Add Text")
-        menu_frame = Frame(text_menu, padx=100, pady=50)
-        menu_frame.grid()
+        text_frame = Frame(text_menu, padx=100, pady=50)
+        text_frame.grid()
 
         # Input box
-        Label(menu_frame, text="Text:").grid(column=1, row=1)
-        text_box = Text(menu_frame, height=5, width=40)
+        Label(text_frame, text="Text:", font=("Ariel", 40, "bold")).grid(column=1, row=1)
+        text_box = Text(text_frame, height=5, width=40)
         text_box.grid(column=2, columnspan=2, row=1)
 
         # Add text button
-        Button(menu_frame, text="Add Text", command=get_user_input).grid(column=2, row=2, sticky="nesw")
+        Button(text_frame, text="Add Text", command=get_user_input).grid(column=2, row=2, sticky="nesw")
 
         # Cancel button
-        Button(menu_frame, text="Cancel", command=text_menu.destroy).grid(column=3, row=2, sticky="nesw")
+        Button(text_frame, text="Cancel", command=text_menu.destroy).grid(column=3, row=2, sticky="nesw")
+    
+    
+    def add_logo(self):
+        """Add logo menu"""
 
 
+        logo_menu = Toplevel(window)
+        logo_menu.title("Add logo")
+        logo_frame = Frame(logo_menu, padx=100, pady=50)
+        logo_frame.grid()
+
+        # Input box
+        Label(logo_frame, text="Logo:", font=("Ariel", 40, "bold")).grid(column=1, row=1)
+        
+        # Canvas for scrollable background
+        # logo_box = Canvas(logo_frame, width=200, height=200, highlightthickness=0)
+        # logo_box.grid(column=2, row=1)
+
+        # list of logo collection
 
 
+        # display logo collection
+        self.img = Image.open("img/logo_collection/logo_1.png")
+        self.img.thumbnail((100, 100))
+        bg = ImageTk.PhotoImage(self.img)
+        logo_1 = Label(logo_frame, image=bg)
+        logo_1.photo = bg
+        logo_1.grid(column=2, row=1)
+
+        # Add text button
+        Button(logo_frame, text="Add Logo").grid(column=2, row=2, sticky="nesw")
+
+        # Cancel button
+        Button(logo_frame, text="Cancel", command=logo_menu.destroy).grid(column=3, row=2, sticky="nesw")
 
 
 window = Tk()
