bd7abdb8d452567790acc747a4f2df14c24feafc
londony11 <londony11@gmail.com>
2023-11-04 06:51:23 +0100

    finish MVP of the app

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageDraw
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+import subprocess
 
 
 class App:
@@ -12,7 +13,6 @@ class App:
     def __init__(self, master):
         master.title("Watermark")
         master.config(padx=100, pady=50)
-        self.master_frame = Frame(master)
 
         # Setup default background
         self.canvas = Canvas(width=200, height=200, highlightthickness=0)
@@ -24,7 +24,7 @@ class App:
         # Setup variables
         self.img_width = 100
         self.img_height = 100
-        self.final_img = Image.new("RGB", (self.img_width, self.img_height), (255, 255, 255, 0))
+        self.img_final = Image.new("RGB", (self.img_width, self.img_height), (255, 255, 255, 0))
 
         # Open Photo button
         Button(text="Open Photo", command=self.open_img).grid(column=1, row=2, columnspan=2, sticky="nesw")
@@ -42,7 +42,7 @@ class App:
     def open_img(self):
         """Image open funciton"""
        
-        # get image path
+        # Get image path
         img_path = filedialog.askopenfilename(initialdir="img/photo_collection")
        
         # Get image, image size
@@ -52,11 +52,13 @@ class App:
         img_pil.thumbnail((self.img_width, self.img_height))
         self.img_width = img_pil.size[0]
         self.img_height = img_pil.size[1]
-       
+
+        # Update final image for saving
+        self.img_final = img_pil
+        # self.final_img.show()
+
         # Display image
         self.img_bg = ImageTk.PhotoImage(img_pil)
-        self.final_img = img_pil
-        # self.final_img.show()
         self.canvas.configure(width=self.img_width, height=self.img_height)
         self.canvas.itemconfig(self.image_container, image=self.img_bg, anchor="nw")
 
@@ -67,15 +69,20 @@ class App:
 
         def add_text():
             """Put text to the image"""
+            
+            # Get user inputs
             user_input = text_box.get(1.0, "end-1c")
             
-            # txt = Image.new("RGBA", (self.img_width, self.img_height), (255, 255, 255, 0))
-            # draw = ImageDraw.Draw(txt)
-            # draw.text((self.img_width/2, self.img_height/2), text=user_input, font=("Ariel", 40, "bold"))
-            # out = Image.self.final_img(self.final_img, txt)
-            # out.show()
+            # Update final image for saving
+            draw = ImageDraw.Draw(self.img_final)
+            font = ImageFont.truetype("Arial Black", 40)
+            text_length = int(draw.textlength(text=user_input, font=font))
+            draw.text((self.img_width-text_length-10, self.img_height-50), text=user_input, font=font)
+            # self.img_final.show()
 
-            watermark_text = self.canvas.create_text(self.img_width/2, self.img_height/2, text=user_input, font=("Ariel", 40, "bold"), anchor="nw")
+            # Display image with text
+            self.canvas.create_text(self.img_width-text_length-10, self.img_height-50, text=user_input, font=("Arial Black", 40), anchor="nw")
+            
 
         text_menu = Toplevel(window)
         text_menu.title("Add Text")
@@ -106,9 +113,11 @@ class App:
         Label(logo_frame, text="Logo:", font=("Ariel", 40, "bold")).grid(column=1, row=1)
 
         # Add logo place
-        self.canvas_logo = Canvas(logo_frame, bg="gray")
-        # logo_bg = ImageTk.PhotoImage(self.img_bg_pil)
-        # self.canvas_logo.create_image(0, 0, image=logo_bg, anchor="nw")
+        self.canvas_logo = Canvas(logo_frame, highlightthickness=0)
+        self.canvas_logo.update()
+        canvas_logo_width = self.canvas_logo.winfo_reqwidth()
+        self.logo_bg = ImageTk.PhotoImage(self.img_bg_pil)
+        self.canvas_logo.create_image((canvas_logo_width-200)/2, 0, image=self.logo_bg, anchor="nw")
         self.canvas_logo.grid(row=1, column=2, columnspan=2)
 
         # Open logo button
@@ -124,15 +133,19 @@ class App:
     def open_logo(self):
         """Image open funciton"""
     
-        # get image path
+        # Get image path
         logo_path = filedialog.askopenfilename(initialdir="img/logo_collection")
     
         # Get image, image size
         self.logo_pil = Image.open(logo_path)
+        self.logo_width = min(self.logo_pil.size[0], 200)
+        self.logo_height = min(self.logo_pil.size[1], 200)
+        self.logo_pil.thumbnail((self.logo_width, self.logo_height))
         self.logo_width = self.logo_pil.size[0]
         self.logo_height = self.logo_pil.size[1]
+        # self.logo_pil.show()
     
-        # Display image
+        # Show logo in menu
         self.logo_bg = ImageTk.PhotoImage(self.logo_pil)
         self.canvas_logo.configure(width=self.logo_width, height=self.logo_height)
         self.logo_container = self.canvas_logo.create_image(0, 0, image=self.logo_bg, anchor="nw")
@@ -141,13 +154,20 @@ class App:
     def add_logo(self):
         """Put logo to the image"""
         
-        watermark_logo = self.canvas.create_image(0, 0, image=self.logo_bg, anchor="nw")
-    
+        # Update final image for saving
+        self.img_final.paste(self.logo_pil, box=((self.img_width - self.logo_width), (self.img_height - self.logo_height)))
+        # self.img_final.show()
 
+        # Display image
+        self.canvas.create_image((self.img_width - self.logo_width), (self.img_height - self.logo_height), image=self.logo_bg, anchor="nw")
+    
+        
     def save_img(self):
         """Save image"""
 
-        pass
+        self.img_final.save("result.jpg")
+        subprocess.call(["open", "result.jpg"])
+
 
 window = Tk()
 app = App(window)
